e6575da686532cddc61fd0e5aa405e4b78cd8266
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-05-04 19:03:24 +0300

    Initial commit

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
-// Your code here
+// utils.js
+export function isPalindrome(word) {
+    function reverse(string) {
+      return string.split("").reverse().join("");
+    }
+  
+    if (!/^[A-Za-z]+$/.test(word)) {
+      throw new Error("Invalid input");
+    }
+    
+    const lowerCaseWord = word.toLowerCase();
+    return lowerCaseWord === reverse(lowerCaseWord);
+  }
+  
